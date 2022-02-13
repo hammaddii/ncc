bfc83305395dbca6923f421d76eefbfe9b62719b
hammaddii <hammadriaz900@gmail.com>
2022-02-13 18:40:35 +0300

    info cards

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
+// menu button for mobile screen
+
 const clsBtn = document.querySelector('.clsBtn');
 const toggleMenu = document.querySelector('.toggleMenu');
 const menu = document.querySelector('.menu');
+
+
 
 toggleMenu.addEventListener('click', () => {
     menu.classList.add('show')
@@ -8,4 +12,18 @@ toggleMenu.addEventListener('click', () => {
 
 clsBtn.addEventListener('click', () => {
     menu.classList.remove('show')
+});
+
+// info page side scroll
+
+const lscrol = document.getElementById('lscrol');
+const rscrol = document.getElementById('rscrol');
+const inforCards = document.querySelector('.inforCards');
+
+rscrol.addEventListener('click', () => {
+    inforCards.scrollLeft += 240;
+});
+
+lscrol.addEventListener('click', () => {
+    inforCards.scrollLeft -= 240;
 });
